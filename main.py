a173e36b4605c03a76c5806eab2fe98e4bacc2ce
yu.yan.wong@comerge.net <yu.yan.wong@comerge.net>
2024-12-05 15:27:13 +0800

    use pygame to get GUI window

---
+++ main.py
@@ -0,0 +1,20 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+from constants import *
+
+def main():
+    pygame.init()
+    print("Starting asteroids!")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, BLACK)
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
